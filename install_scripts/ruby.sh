bccaa9129e0dfa5d459dcc9c706d5675baac5f39
Sean Watkins <slwatkins@uh.edu>
2018-08-23 17:58:35 -0500

    Update ruby

--- install_scripts/ruby.sh
+++ install_scripts/ruby.sh
@@ -11,11 +11,11 @@ if [ -f "$SHARED_DIR/install_scripts/config" ]; then
 fi
 
 cd $HOME_DIR
-gpg2 --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash -s stable
 source $HOME_DIR/.rvm/scripts/rvm
 
-rvm install 2.3.1
-rvm  --default use 2.3.1
+rvm install 2.4.4
+rvm  --default use 2.4.4
 
 echo "Done"
