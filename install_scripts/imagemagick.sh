f2dc36f722da3e4c377c8abec03138ed231e71d7
Sean Watkins <slwatkins@uh.edu>
2018-08-30 09:21:56 -0500

    Update ImageMagick version to 6.9.10-11

--- install_scripts/imagemagick.sh
+++ install_scripts/imagemagick.sh
@@ -10,16 +10,16 @@ if [ -f "$SHARED_DIR/install_scripts/config" ]; then
   . $SHARED_DIR/install_scripts/config
 fi
 
-if [ ! -f "$DOWNLOAD_DIR/ImageMagick-6.9.10-10.tar.gz" ]; then
+if [ ! -f "$DOWNLOAD_DIR/ImageMagick-6.9.10-11.tar.gz" ]; then
   echo -n "Downloading ImageMagic..."
-  wget -q "https://www.imagemagick.org/download/ImageMagick-6.9.10-10.tar.gz" -O "$DOWNLOAD_DIR/ImageMagick-6.9.10-10.tar.gz"
+  wget -q "https://www.imagemagick.org/download/ImageMagick-6.9.10-11.tar.gz" -O "$DOWNLOAD_DIR/ImageMagick-6.9.10-10.tar.gz"
   echo " done"
 fi
 
-cp $DOWNLOAD_DIR/ImageMagick-6.9.10-10.tar.gz /tmp
+cp $DOWNLOAD_DIR/ImageMagick-6.9.10-11.tar.gz /tmp
 cd /tmp
-tar -xzf ImageMagick-6.9.10-10.tar.gz
-cd ImageMagick-6.9.10-10
+tar -xzf ImageMagick-6.9.10-11.tar.gz
+cd ImageMagick-6.9.10-11
 ./configure --with-openjp2=yes
 make && make install
 ldconfig /usr/local/lib
